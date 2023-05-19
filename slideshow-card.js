7139c8f99948c9cb7c72946f3fa2311af0becb0e
viotemp1 <viotemp1@icloud.com>
2023-05-19 19:04:11 +0300

    Update slideshow-card.js

--- slideshow-card.js
+++ slideshow-card.js
@@ -1,8 +1,8 @@
 // import {Polymer} from '@polymer/polymer';
-// import {Polymer} from "https://unpkg.com/browse/@polymer/polymer@3.0.3/polymer-legacy.js?module";
-import { PolymerElement, html } from 'https://unpkg.com/@polymer/polymer@3.0.0/polymer-element.js?module';
-//class SlideshowCard extends Polymer.Element {
-class SlideshowCard extends PolymerElement {
+import {Polymer} from "https://unpkg.com/browse/@polymer/polymer@3.0.3/polymer-legacy.js?module";
+// import { PolymerElement, html } from 'https://unpkg.com/@polymer/polymer@3.0.0/polymer-element.js?module';
+class SlideshowCard extends Polymer.Element {
+// class SlideshowCard extends PolymerElement {
   myPromise = new Promise((resolve) => { this.resolve = resolve });
 
   constructor() {
